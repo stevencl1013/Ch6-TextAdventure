67492871441afca2001d11b2f5e77b7e79c5caf4
Steven Lee <G00238962@W213-27.academic.raritanval.edu>
2016-03-28 12:12:26 -0400

    Finished Start of Game
    Creates rooms, sets their exits, puts master key in a random room.

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
@@ -19,6 +21,7 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
+    private boolean hasMasterKey = false;
         
     /**
      * Create the game and initialise its internal map.
@@ -34,30 +37,62 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room[] rooms = new Room[15];
+        Random rand = new Random();
+        int masterKeyRoom = 11;
+        while(masterKeyRoom == 11) // randomly set master key room to any room except the neighbor's house.
+        {
+            masterKeyRoom = rand.nextInt(15);
+        }
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        rooms[0] = new Room("Woods");
+        rooms[1] = new Room("Lake");
+        rooms[2] = new Room("Backyard");
+        rooms[3] = new Room("West Yard");
+        rooms[4] = new Room("Living Room");
+        rooms[5] = new Room("Hall");
+        rooms[6] = new Room("Kitchen");
+        rooms[7] = new Room("East Yard");
+        rooms[8] = new Room("Main Bathroom");
+        rooms[9] = new Room("Front Yard");
+        rooms[10] = new Room("Closet");
+        rooms[11] = new Room("Neighbor's House");
+        rooms[12] = new Room("Basement");
+        rooms[13] = new Room("Bedroom");
+        rooms[14] = new Room("Upper Bathroom");
         
         // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
+        rooms[0].setExit("east", rooms[1]);
+        rooms[0].setExit("south", rooms[2]);
+        rooms[1].setExit("south", rooms[7]);
+        rooms[2].setExit("north", rooms[0]);
+        rooms[2].setExit("south", rooms[5]);
+        rooms[3].setExit("east", rooms[4]);
+        rooms[4].setExit("west", rooms[3]);
+        rooms[4].setExit("south", rooms[8]);
+        rooms[4].setExit("east", rooms[5]);
+        rooms[5].setExit("north", rooms[2]);
+        rooms[5].setExit("west", rooms[4]);
+        rooms[5].setExit("south", rooms[9]);
+        rooms[5].setExit("east", rooms[6]);
+        rooms[5].setExit("upstairs", rooms[13]);
+        rooms[5].setExit("downstairs", rooms[12]);
+        rooms[6].setExit("west", rooms[5]);
+        rooms[6].setExit("south", rooms[10]);
+        rooms[6].setExit("east", rooms[7]);
+        rooms[7].setExit("north", rooms[1]);
+        rooms[7].setExit("west", rooms[6]);
+        rooms[8].setExit("north", rooms[4]);
+        rooms[9].setExit("north", rooms[5]);
+        rooms[10].setExit("north", rooms[6]);
+        // room 11 is only accessible with master key.
+        rooms[12].setExit("upstairs", rooms[9]);
+        rooms[13].setExit("downstairs", rooms[9]);
+        rooms[13].setExit("east", rooms[14]);
+        rooms[14].setExit("west", rooms[13]);
 
-        theater.setExit("west", outside);
-
-        pub.setExit("east", outside);
-
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
-
-        currentRoom = outside;  // start game outside
+        currentRoom = rooms[9];  // start game in the front yard.
     }
 
     /**
